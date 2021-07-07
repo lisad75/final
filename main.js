cee5322fec4146f6b6036c280e6dfa0970542917
Lisa Davidson <lisadavidson@Lisas-Air.phub.net.cable.rogers.com>
2021-07-07 19:56:16 -0400

    file change

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 main.js
+function hideModal() {
+	modal.classList.toggle('hidden');
+}
+hideModal();
+document.getElementById('sayHi').addEventListener('click', hideModal);
+document.getElementById('cancel').addEventListener('click', hideModal);
+document.getElementById('submit').addEventListener('click', hideModal);
